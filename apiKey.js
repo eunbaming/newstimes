f51a959480cbac1376ccc94cacecdd20687a3da1
kimyuna <dbskqnsghd@naver.com>
2023-02-28 15:34:17 +0900

    code refactoring and environment variable setting

---
+++ apiKey.js
@@ -0,0 +1,3 @@
+const config = {
+	apiKey: "hUPqLD-FjrLIR9GPpwZOJh4iClTmoxZLnjczShd_jqg"
+}
--- main.js
+++ main.js
@@ -1,71 +1,67 @@
-let news = []
-let page = 1
-let total_pages = 0
-let menus = document.querySelectorAll(".menus button")
-menus.forEach((menu)=>menu.addEventListener("click", (event)=>getNewsByTopic(event)))
-let sideMenus = document.querySelectorAll(".side-menu-list button")
-sideMenus.forEach((menu)=>menu.addEventListener("click", (event)=>getNewsByTopic(event)))
-let searchButton = document.getElementById("search-btn")
-let url
+let news = [];
+let page = 1;
+let total_pages = 0;
+let searchButton = document.getElementById("search-btn");
+let url;
 
-document.getElementById("search-input").addEventListener("keyup", (event) => {
+let menus = document.querySelectorAll(".menus button");
+menus.forEach((menu)=>menu.addEventListener("click", (event)=>getNewsByTopic(event)));
+
+let sideMenus = document.querySelectorAll(".side-menu-list button");
+sideMenus.forEach((menu)=>menu.addEventListener("click", (event)=>getNewsByTopic(event)));
+
+document.getElementById("search-input").addEventListener("keydown", (event) => {
   if (event.keyCode === 13){
     getNewsByKeyword(event)
-  }
-})
+  };
+});
 
 function pageReload() {
-  location.reload()
-}
+  location.reload();
+};
 
 const getNews = async()=>{
   try {
-    let header = new Headers({"x-api-key" : "lo7L8FrJHAJHfobdGclfo38dr6LUzsqhfOABD3ihWl0"})
-    url.searchParams.set('page', page) // &page=page
-    // console.log(url)
-    let response = await fetch(url,{headers : header})
-    let data = await response.json()
-    console.log("data는", data)
-    console.log("response는", response)
+    let header = new Headers({"x-api-key" : config.apiKey});
+    url.searchParams.set('page', page);
+    let response = await fetch(url,{headers : header});
+    let data = await response.json();
     if(response.status == 200){
       if(data.total_hits == 0){
-        throw new Error("No matches for your search")
-      }
-      news = data.articles
-      page = data.page
-      total_pages = data.total_pages
-      render()
-      pagination()
+        throw new Error("No matches for your search");
+      };
+      news = data.articles;
+      page = data.page;
+      total_pages = data.total_pages;
+      render();
+      pagination();
     }else {
-      throw new Error(data.message)
-    }
+      throw new Error(data.message);
+    };
   }catch(error) {
-    console.log("에러 내용은", error.message)
-    errorRender(error.message)
-  }
-}
+    errorRender(error.message);
+  };
+};
 
 const getLatesNews = async()=>{
-  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=10`)
-  getNews()
-}
+  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=10`);
+  getNews();
+};
 
 const getNewsByTopic = async(event)=>{
-  // console.log(event.target.textContent)
-  let topic = event.target.textContent.toLowerCase()
+  let topic = event.target.textContent.toLowerCase();
   url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=${topic}&page_size=10`)
-  getNews()
-}
+  getNews();
+};
 
 const getNewsByKeyword = async()=>{
-  let keyword = document.getElementById("search-input").value
-  // console.log("keyword", keyword)
-  url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&countries=US&page_size=10`)
-  getNews()
-}
+  let keyword = document.getElementById("search-input").value;
+  url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&countries=US&page_size=10`);
+  getNews();
+};
 
 const render = ()=>{
-  let newsHTML = ""
+  let newsHTML = "";
   newsHTML = news.map((item)=>{
     return `<div class="row news">
     <div class="col-lg-4">
@@ -80,33 +76,32 @@ const render = ()=>{
           :item.summary.length > 200
           ?item.summary.substr(0,200) + "..."
           :item.summary
-        }
+        };
       </p>
       <div>
-        ${item.author || "no author"} * ${moment(item.published_date).fromNow()}
+        ${item.author || "no author"} * ${moment(item.published_date).fromNow()};
       </div>
     </div>
   </div>`
-  }).join("")
+  }).join("");
 
-  document.getElementById("news-board").innerHTML = newsHTML
-}
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
 
 const errorRender = (message)=>{
-  let errorHTML = `<div class="alert alert-danger text-center" role="alert">${message}
-</div>`
+  let errorHTML = `<div class="alert alert-danger text-center" role="alert">${message}</div>`;
 
-  document.getElementById("news-board").innerHTML = errorHTML
-}
+  document.getElementById("news-board").innerHTML = errorHTML;  
+};
 
 const pagination = ()=>{
-  let paginationHTML = ``
-  let pageGroup = Math.ceil(page / 5)
-  let last = pageGroup * 5
+  let paginationHTML = ``;
+  let pageGroup = Math.ceil(page / 5);
+  let last = pageGroup * 5;
   if(last > total_pages){
-    last = total_pages
-  }
-  let first = last - 4 <= 0 ? 1 : last - 4
+    last = total_pages;
+  };
+  let first = last - 4 <= 0 ? 1 : last - 4;
 
   if(first >= 6) {
     paginationHTML = `<li class="page-item">
@@ -114,12 +109,12 @@ const pagination = ()=>{
 </li>
   <li class="page-item">
 <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${page-1})">&lt;</a>
-</li>`
-  }
+</li>`;
+  };
 
   for(let i=first; i<=last; i++){
-    paginationHTML += `<li class="page-item ${page===i?"active":""}"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`
-  }
+    paginationHTML += `<li class="page-item ${page===i?"active":""}"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`;
+  };
 
   if(last < total_pages) {
     paginationHTML += `<li class="page-item">
@@ -127,33 +122,33 @@ const pagination = ()=>{
 </li>
   <li class="page-item">
 <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(total_pages)">&gt;&gt;</a>
-</li>`
-  }
+</li>`;
+  };
 
-  document.querySelector(".pagination").innerHTML = paginationHTML
-}
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+};
 
 const moveToPage = (pageNum)=>{
-  page = pageNum
-  console.log(page)
-  getNews()
-}
+  page = pageNum;
+  console.log(page);
+  getNews();
+};
 
 function openNav(){
-  document.getElementById("mySideNav").style.width = "250px"
-}
+  document.getElementById("mySideNav").style.width = "250px";
+};
 function closeNav(){
-  document.getElementById("mySideNav").style.width = "0"
-}
+  document.getElementById("mySideNav").style.width = "0";
+};
 function openSearchBox(){
-  let searchArea = document.getElementById("search-area")
+  let searchArea = document.getElementById("search-area");
   if(searchArea.style.display === "inline"){
-    searchArea.style.display = "none"
+    searchArea.style.display = "none";
   }else {
-    searchArea.style.display = "inline"
-  }
-}
+    searchArea.style.display = "inline";
+  };
+};
 
 
-searchButton.addEventListener("click", getNewsByKeyword)
-getLatesNews()
+searchButton.addEventListener("click", getNewsByKeyword);
+getLatesNews();
